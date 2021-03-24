563c22016ccbb56fd2dfdd53d85d5b0fe3c7f19c
ffifee <bukovicmatej@gmail.com>
2021-03-24 15:25:19 +0100

    commiting a project

---
+++ JavaScript.js
@@ -0,0 +1,20 @@
+﻿
+var backgrounds = ["images/backgroundImage0",
+                           "images/backgroundImage1",
+                           "images/backgroundImage2",
+                           "images/backgroundImage3",
+                           "images/backgroundImage4",
+                           "images/backgroundImage5",
+                           "images/backgroundImage6",
+                           "images/backgroundImage7",
+                           "images/backgroundImage8",
+                           "images/backgroundImage9",
+                           "images/backgroundImage10",
+                           "images/backgroundImage11", ]
+function randomBackgroundImage(){
+    var rand = Math.floor((Math.random() * 10) + 1);
+    var randToString = rand.toString();
+    var filePath = "url('" + "images/backgroundImage" + randToString + ".jpg" + "')";
+    var x = document.getElementsByClassName("fullScreen-bg-img");
+    x[0].style.backgroundImage = filePath;
+}
